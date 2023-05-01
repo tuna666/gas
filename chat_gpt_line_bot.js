e303d687dbedce5ac14b6ed737c8b1316a06e22f
tuna <ev0.kpx@gmail.com>
2023-05-01 12:14:06 +0900

    プロンプトを参照する機能を追加

--- chat_gpt_line_bot.js
+++ chat_gpt_line_bot.js
@@ -3,24 +3,29 @@ const GPT_API_KEY = 'YOURKEY';
 const SPREADSHEET_ID = 'YOURID';
 const MODEL = "gpt-3.5-turbo";
 
-// スプレッドシートを取得する関数
-function getSpreadsheet() {
-  return SpreadsheetApp.openById(SPREADSHEET_ID);
+function getHistorySheet() {
+  const spreadsheet = SpreadsheetApp.openById(SPREADSHEET_ID);
+  const sheet = spreadsheet.getSheetByName('History');
+  return sheet;
+}
+
+function getPromptSheet() {
+  const spreadsheet = SpreadsheetApp.openById(SPREADSHEET_ID);
+  const sheet = spreadsheet.getSheetByName('Prompts');
+  return sheet;
 }
 
 function doPost(e) {
   try {
     const json = JSON.parse(e.postData.contents);
     const events = json.events;
-
     events.forEach(function (event) {
-      const userId = event.source.userId; // ユーザーIDを取得
       switch (event.type) {
         case 'message':
-          handleMessageEvent(event, userId);
+          handleMessageEvent(event);
           break;
         case 'postback':
-          handlePostbackEvent(event, userId);
+          handlePostbackEvent(event);
           break;
       }
     });
@@ -31,22 +36,108 @@ function doPost(e) {
   return ContentService.createTextOutput(JSON.stringify({ content: 'success' })).setMimeType(ContentService.MimeType.JSON);
 }
 
-
-function handleMessageEvent(event, userId) {
+function handleMessageEvent(event) {
+  const userId = event.source.userId;
   const userMessage = event.message.text;
   const replyToken = event.replyToken;
+  // const userId = 'Ua91b05cac5dd461d9b7d2905ad843c46'
+  // const userMessage = '#promptKoreanMom'
+  // const replyToken = 111
 
-  const chatGptResponse = chatWithGpt(userMessage, userId);
-  replyMessage(replyToken, chatGptResponse);
+  if (userMessage.startsWith('#prompt')) {
+    handlePromptSelection(userMessage, userId);
+    replyMessage(replyToken, 'プロンプトをセットしました。会話を続けてください。');
+  } else if (userMessage === '#reset context') {
+    saveResetDate(userId);
+    replyMessage(replyToken, '過去の会話をリセットしました。新しい会話を始めてください。');
+  } else if (userMessage === '#clear') {
+    clearSheetContentsExceptHeader();
+    replyMessage(replyToken, 'スプシをクリアしました');
+  } else {
+    const chatGptResponse = chatWithGpt(userMessage, userId);
+    replyMessage(replyToken, chatGptResponse);
+  }
 }
 
+function handlePromptSelection(userMessage, userId) {
+  const promptId = userMessage.substring(1); // #を除いたプロンプトIDを取得
+  const promptText = getPromptText(promptId);
+  if (promptText) {
+    savePrompt(userId, promptText);
+  }
+}
+
+function getPromptText(promptId) {
+  const sheet = getPromptSheet();
+  const numRows = sheet.getLastRow();
+  const searchRange = sheet.getRange(1, 1, numRows, 2);
+  const values = searchRange.getValues();
+
+  for (let i = 0; i < values.length; i++) {
+    if (values[i][0] === promptId) {
+      return values[i][1];
+    }
+  }
+
+  return null;
+}
+
+function getSavedPrompt(userId) {
+  const sheet = getHistorySheet()
+  const numRows = sheet.getLastRow();
+  const searchRange = sheet.getRange(1, 1, numRows, 5);
+  const values = searchRange.getValues();
+  let latestPrompt = null;
+
+  for (let i = 0; i < values.length; i++) {
+    if (values[i][0] === userId && values[i][4] === "prompt") {
+      latestPrompt = values[i][1];
+    }
+  }
+
+  if (latestPrompt) {
+    return latestPrompt;
+  }
+
+  return 'あなたは助けになるアシスタントです。';
+}
+
+function savePrompt(userId, prompt) {
+  const sheet = getHistorySheet()
+  const currentDate = new Date();
+  sheet.appendRow([userId, prompt, "", "", "prompt", currentDate]);
+}
+
+function saveResetDate(userId) {
+  const sheet = getHistorySheet()
+  const currentDate = new Date();
+  sheet.appendRow([userId, "", "", "", "reset_date", currentDate]);
+}
+
+function getResetDate(userId) {
+  const sheet = getHistorySheet()
+  const numRows = sheet.getLastRow();
+  const searchRange = sheet.getRange(1, 1, numRows, 6);
+  const values = searchRange.getValues();
+  let latestResetDate = null;
+
+  // 最新のリセット日付を取得
+  for (let i = 0; i < values.length; i++) {
+    if (values[i][0] === userId && values[i][4] === "reset_date") {
+      latestResetDate = values[i][5];
+    }
+  }
+
+  if (latestResetDate) {
+    return new Date(latestResetDate);
+  }
+
+  return null;
+}
 
 function chatWithGpt(userMessage, userId) {
-  const previousMessages = getPreviousMessages(userId);
-
-  if (!userMessage || (typeof userMessage === 'string' && userMessage.trim() === '')) {
-    return 'メッセージが空です。';
-  }
+  const prompt = getSavedPrompt(userId);
+  const resetDate = getResetDate(userId);
 
   const url = 'https://api.openai.com/v1/chat/completions';
   const headers = {
@@ -57,22 +148,39 @@ function chatWithGpt(userMessage, userId) {
   const messages = [
     {
       'role': 'system',
-      'content': 'You are a helpful assistant.'
+      'content': prompt
     },
-    ...previousMessages,
-    {
-      'role': 'user',
-      'content': userMessage
-    }
   ];
+
+  const userSheet = getHistorySheet()
+  const numRows = userSheet.getLastRow();
+  const searchRange = userSheet.getRange(1, 1, numRows, 6);
+  const values = searchRange.getValues();
+
+  for (let i = 0; i < values.length; i++) {
+    if (values[i][0] === userId && (!resetDate || new Date(values[i][5]) >= resetDate)) {
+      messages.push({
+        'role': 'user',
+        'content': values[i][2],
+      }, {
+        'role': 'assistant',
+        'content': values[i][3],
+      });
+    }
+  }
+
+  messages.push({
+    'role': 'user',
+    'content': userMessage,
+  });
 
   const payload = {
     'model': 'gpt-3.5-turbo',
-    'messages': messages,
-    'max_tokens': 1024,
+    'messages': messages.slice(-1000), // 最新の1000トークンまで取得
+    'max_tokens': 1000,
     'n': 1,
     'stop': null,
-    'temperature': 0.8,
+    'temperature': 0.7,
   };
 
   const options = {
@@ -82,53 +190,35 @@ function chatWithGpt(userMessage, userId) {
   };
 
   const response = UrlFetchApp.fetch(url, options);
-  const jsonResponse = JSON.parse(response.getContentText());
-  const chatGptResponse = jsonResponse.choices[0].message.content;
+  const json = JSON.parse(response.getContentText());
+  console.log('OpenAI APIレスポンス:', json);
+  const assistantMessage = json.choices[0].message.content;
 
-  saveMessageToSpreadsheet(userId, userMessage, chatGptResponse);
-  return chatGptResponse;
+  saveUserAndAssistantMessages(userId, userMessage, assistantMessage);
+
+  return assistantMessage;
 }
 
-function getPreviousMessages(userId) {
-  const sheet = SpreadsheetApp.openById(SPREADSHEET_ID).getActiveSheet();
-  const data = sheet.getDataRange().getValues();
-  const previousMessages = [];
-
-  for (const row of data) {
-    if (row[0] === userId) {
-      previousMessages.push({
-        'role': 'user',
-        'content': row[1]
-      });
-      previousMessages.push({
-        'role': 'assistant',
-        'content': row[2]
-      });
-    }
-  }
-
-  return previousMessages;
+function saveUserAndAssistantMessages(userId, userMessage, assistantMessage) {
+  const currentDate = new Date();
+  const sheet = getHistorySheet()
+  sheet.appendRow([userId, "", userMessage, assistantMessage, "", currentDate]);
 }
 
-function saveMessageToSpreadsheet(userId, userMessage, assistantMessage) {
-  const sheet = SpreadsheetApp.openById(SPREADSHEET_ID).getActiveSheet();
-  sheet.appendRow([userId, userMessage, assistantMessage]);
-}
 
-function replyMessage(replyToken, chatGptResponse) {
+function replyMessage(replyToken, message) {
   const url = 'https://api.line.me/v2/bot/message/reply';
   const headers = {
     'Content-Type': 'application/json; charset=UTF-8',
     'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN,
   };
+
   const payload = {
     'replyToken': replyToken,
-    'messages': [
-      {
-        'type': 'text',
-        'text': chatGptResponse,
-      },
-    ],
+    'messages': [{
+      'type': 'text',
+      'text': message
+    }]
   };
 
   const options = {
@@ -139,3 +229,15 @@ function replyMessage(replyToken, chatGptResponse) {
 
   UrlFetchApp.fetch(url, options);
 }
+
+function clearSheetContentsExceptHeader() {
+  const sheet = getHistorySheet()
+  const numRows = sheet.getLastRow();
+  const numColumns = sheet.getLastColumn();
+
+  if (numRows > 1) {
+    const rangeToClear = sheet.getRange(2, 1, numRows - 1, numColumns);
+    rangeToClear.clearContent();
+  }
+}
+
